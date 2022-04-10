5b07d41e0f24c8dc9547e07119af40880dd1bbb7
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-04-10 21:12:52 +0600

    Github Signin integrated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase_init';
-import {getAuth} from 'firebase/auth';
+import {getAuth, GithubAuthProvider} from 'firebase/auth';
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from 'react';
 
@@ -9,9 +9,10 @@ const auth=getAuth(app);
 
 function App() {
   const [user,setUser]=useState({});
-  const provider = new GoogleAuthProvider();
-  const signInHandler=()=>{
-    signInWithPopup(auth, provider)
+  const providerGoogle = new GoogleAuthProvider();
+  const providerGithub=new GithubAuthProvider();
+  const signInWithGoogle=()=>{
+    signInWithPopup(auth, providerGoogle)
       .then((result) => {
         const user=result.user;
         // console.log(user);
@@ -21,6 +22,17 @@ function App() {
         console.log(error);
       });
   }
+  const signInWithGithub=()=>{
+    signInWithPopup(auth,providerGithub)
+    .then((result)=>{
+      const user=result.user;
+      setUser(user);
+    })
+    .catch((error)=>{
+      console.error(error);
+    })
+  }
+
 
   const signOutHandler=()=>{
     const auth = getAuth();
@@ -37,7 +49,8 @@ function App() {
   return (
     <div className="App">
       <h1>Firebase Authentication</h1>
-      <button onClick={signInHandler}>Sign in With Google</button>
+      <button onClick={signInWithGoogle}>Sign in With Google</button>
+      <button onClick={signInWithGithub}>Sign in With Github</button>
       <button onClick={signOutHandler}>Sign Out</button>
 
       <h2>Name:{user.displayName}</h2>
